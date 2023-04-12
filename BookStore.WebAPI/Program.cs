ae98dc8caf94cbef034f6f41fa055d02d0a4e85f
Birol AYDIN <57332230+BirolAYDIN@users.noreply.github.com>
2023-04-12 23:19:07 +0300

    Book List Added.

--- BookStore.WebAPI/Program.cs
+++ BookStore.WebAPI/Program.cs
@@ -1,11 +1,33 @@
+using Microsoft.OpenApi.Models;
+using System.Reflection;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+
+
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+
+
+builder.Services.AddSwaggerGen(options =>
+{
+    options.SwaggerDoc("v1", new OpenApiInfo
+    {
+        Version = "v1",
+        Title = "BookStore.WebAPI",
+        Description = "An ASP.NET Core Web API",
+        Contact = new OpenApiContact
+        {
+            Name = "Github Repository",
+            Url = new Uri("https://github.com/BirolAYDIN/BookStore.WebAPI")
+        }
+    });
+
+    // using System.Reflection;
+    var xmlFilename = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
+    options.IncludeXmlComments(Path.Combine(AppContext.BaseDirectory, xmlFilename));
+});
+
 
 var app = builder.Build();
 
